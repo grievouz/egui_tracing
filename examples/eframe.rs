f3dd9f2586afa937cccc3d3fac3303476d51cdba
Peter Kristensen <peter@ptx.dk>
2024-04-20 20:04:25 +0200

    chore: update `egui` to version 0.27 and `tracing-log` to version 0.2

--- examples/eframe.rs
+++ examples/eframe.rs
@@ -10,8 +10,8 @@ fn main() {
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_resizable(true)
-            .with_inner_size(egui::vec2(800.0, 500.0)),
+            .with_inner_size(egui::vec2(800.0, 500.0))
+            .with_resizable(true),
         ..Default::default()
     };
     eframe::run_native(
@@ -19,7 +19,7 @@ fn main() {
         options,
         Box::new(|_cc| Box::new(MyApp::new(collector))),
     )
-    .unwrap();
+        .unwrap();
 }
 
 pub struct MyApp {
