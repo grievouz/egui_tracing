74995752989ec38c8dd3bea348a884457102e349
Tammo Ronke <ronke.tammo@gmail.com>
2026-04-08 21:39:47 -0700

    feat: allow target menu to close on click outside

--- egui-tracing/src/ui/components/target_menu_button.rs
+++ egui-tracing/src/ui/components/target_menu_button.rs
@@ -1,4 +1,4 @@
-use egui::Ui;
+use egui::{PopupCloseBehavior, Ui};
 use globset::Glob;
 
 use super::target_menu_item::TargetMenuItem;
@@ -17,34 +17,38 @@ impl<'a> TargetMenuButton<'a> {
 
     pub fn show(self, ui: &mut Ui) {
         let state = self.state.unwrap();
-        ui.menu_button("Target", |ui| {
-            ui.label("Target Filter");
+        let button = ui.button("Target");
 
-            let (input, add_button) = ui
-                .horizontal(|ui| {
-                    let input = ui
-                        .text_edit_singleline(&mut state.input)
-                        .on_hover_text("example: eframe::*");
-                    let button = ui.button("Add");
-                    (input, button)
-                })
-                .inner;
+        egui::Popup::menu(&button)
+            .close_behavior(PopupCloseBehavior::CloseOnClickOutside)
+            .show(|ui| {
+                ui.label("Target Filter");
 
-            if add_button.clicked()
-                || (input.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)))
-            {
-                state.targets.push(Glob::new(&state.input).unwrap());
-                state.input = "".to_owned();
-            }
-
-            for (i, target) in state.targets.clone().iter().enumerate() {
-                TargetMenuItem::default()
-                    .on_clicked(|| {
-                        state.targets.remove(i);
+                let (input, add_button) = ui
+                    .horizontal(|ui| {
+                        let input = ui
+                            .text_edit_singleline(&mut state.input)
+                            .on_hover_text("example: eframe::*");
+                        let add = ui.button("Add");
+                        (input, add)
                     })
-                    .target(target)
-                    .show(ui);
-            }
-        });
+                    .inner;
+
+                if add_button.clicked()
+                    || (input.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)))
+                {
+                    state.targets.push(Glob::new(&state.input).unwrap());
+                    state.input = "".to_owned();
+                }
+
+                for (i, target) in state.targets.clone().iter().enumerate() {
+                    TargetMenuItem::default()
+                        .on_clicked(|| {
+                            state.targets.remove(i);
+                        })
+                        .target(target)
+                        .show(ui);
+                }
+            });
     }
 }
